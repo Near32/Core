033c2cebab556444bc6147d6d8abb141d3408b62
Near32 <denamganai.kevin@gmail.com>
2016-01-23 11:08:33 +0100

    compilation tested on RPI, few changes made...

--- ROBOT/ServoEXP/ServoEXP.cpp
+++ ROBOT/ServoEXP/ServoEXP.cpp
@@ -1,6 +1,6 @@
 #include "ServoEXP.h"
 
-ServoEXP::ServoEXP(const VAR& variable_, int pin_, int idx, const Mat<float>& se3Init, float zerooffset_, float min, float max) : Servo(, int pin_, int idx, const Mat<float>& se3Init, float zerooffset_, float min, float max)
+ServoEXP::ServoEXP(const VAR& variable_, int pin_, int idx, const Mat<float>& se3Init, float zerooffset_, float min, float max) : Servo( int pin_, int idx, const Mat<float>& se3Init, float zerooffset_, float min, float max)
 {
 		frameEXP = new FrameEXP(variable_, idx, se3Init);
 		
