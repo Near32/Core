ae8deb6d286061bef4d2110f4c0d29089d8e2cf9
Near32 <denamganai.kevin@gmail.com>
2016-01-25 17:36:47 +0100

    Gestion des commandes...

--- ROBOT/mainHAROEXP.cpp
+++ ROBOT/mainHAROEXP.cpp
@@ -2,6 +2,11 @@
 
 int main( int argc, char* argv[])
 {
+	std::cout << "Raspberry Pi : initialization of wiringPi : ..." << std::endl;
+	if( wiringPiSetup() == -1)
+		exit(-1);
+	std::cout << "Raspberry Pi : initialization of wiringPi : OKAY." << std::endl;
+	
 	HAROEXP instanceHARO;
 	
 	instanceHARO.loop();
