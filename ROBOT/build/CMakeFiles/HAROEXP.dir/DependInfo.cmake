18d222c75bb91562084fb2b8f5c9487498c5ebb5
Near32 <denamganai.kevin@gmail.com>
2016-02-02 10:15:15 +0000

    initial configuration revised. + CMAKE

---
+++ ROBOT/build/CMakeFiles/HAROEXP.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/Frame/Frame.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/Frame/Frame.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/FrameEXP/FrameEXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/FrameEXP/FrameEXP.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/HARO/HAROEXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/HARO/HAROEXP.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/HARO/HAROLegsEXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/HARO/HAROLegsEXP.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/MVG/MVG.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/MVG/MVG.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/PE/MPU6050/MPU6050.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/PE/MPU6050/MPU6050.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/PE/PE.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/PE/PE.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/PE/RPI/I2Cdev/I2Cdev.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/PE/RPI/I2Cdev/I2Cdev.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/Quaternion/Quaternion.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/Quaternion/Quaternion.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/Servo/Servo.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/Servo/Servo.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/ServoEXP/ServoEXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/ServoEXP/ServoEXP.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/EXP/EXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/home/pi/git/git/PROJECTS/Core/Core/EXP/EXP.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/FUNC/FUNC.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/home/pi/git/git/PROJECTS/Core/Core/FUNC/FUNC.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/Operations/Operations.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/home/pi/git/git/PROJECTS/Core/Core/Operations/Operations.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/VAR/VAR.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/home/pi/git/git/PROJECTS/Core/Core/VAR/VAR.cpp.o"
+  "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/mainHAROEXP.cpp" "/home/pi/git/git/PROJECTS/Core/Core/ROBOT/build/CMakeFiles/HAROEXP.dir/mainHAROEXP.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
