6bec97eb34eeffcd9fc7748b7543bdd0b2203580
Near32 <denamganai.kevin@gmail.com>
2016-02-05 17:21:22 +0100

    TRAJECTORY FOLOWING : in progress...

--- ROBOT/HARO/HAROEXP.cpp
+++ ROBOT/HARO/HAROEXP.cpp
@@ -301,7 +301,7 @@ void HAROEXP::generateTrajectories()
 	
 	std::cout << " ... " ;
 	//final desired position :
-	for(int i=nbrTraj;i--;)	trajectories[i] = operatorL( trajectories[i], EXP2floatM( (  harolegs->*(idxTraj2r[i]) )() )  + add);
+	for(int i=nbrTraj;i--;)	trajectories[i] = operatorL( trajectories[i], extract( EXP2floatM( (  harolegs->*(idxTraj2r[i]) )() ), 1,1, 3,1)  + add);
 	
 	std::cout << " OKAY." << std::endl;
 	//TODO
